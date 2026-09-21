4718e854bbd0543b48704c406d196c9619438882
Andrew Espinosa <adespinosa14@users.noreply.github.com>
2026-09-20 18:53:07 -0700

    Add: daily-temperatures - submission-8

---
+++ Data Structures & Algorithms/daily-temperatures/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            s_struct = [t, i]
+            while stack and t > stack[-1][0]:
+                val = stack.pop()
+                res[val[1]] = i - val[1]
+            stack.append(s_struct)
+
+        return res
